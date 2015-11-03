13d3b73ecefc6a14bdb866652aa5af7b74378730
Eric Tran <erictritran@yahoo.com>
2015-11-03 14:47:38 -0800

    Text color changing randomly for Hello World

--- CruApp/ViewController.swift
+++ CruApp/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTextLabel.text = "Hello World!"
+        print("Hello World!")
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.25, target: self, selector: "changeColor", userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func changeColor() {
+        let red = Float(arc4random()) / Float(UINT32_MAX)
+        let green = Float(arc4random()) / Float(UINT32_MAX)
+        let blue = Float(arc4random()) / Float(UINT32_MAX)
+        myTextLabel.textColor = UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: 1.0)
+    }
 
 }
 

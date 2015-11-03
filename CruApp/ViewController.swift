fd71740eae9fa1e8dd952a9a58bb6dfabad891f9
Eric Tran <erictritran@yahoo.com>
2015-11-03 14:14:28 -0800

    Initial Commit

---
+++ CruApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CruApp
+//
+//  Created by Eric Tran on 11/3/15.
+//  Copyright © 2015 iCrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

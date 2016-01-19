5077bccbf8a6ac3c74a1214b9be068ba41b34c49
takong15 <takong15@gmail.com>
2016-01-18 19:35:25 -0800

    images for events

---
+++ CruApp/MissionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MissionViewCell.swift
+//  CruApp
+//
+//  Created by Tammy Kong on 1/18/16.
+//  Copyright © 2016 iCrew. All rights reserved.
+//
+
+import UIKit
+
+class MissionViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

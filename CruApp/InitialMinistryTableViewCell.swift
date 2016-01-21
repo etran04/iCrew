9b93add7913af489d9b9731e315b6babd87e2945
mariel.k.sanchez@gmail.com <mariel.k.sanchez@gmail.com>
2016-01-20 22:20:13 -0800

    commiting intial views

---
+++ CruApp/InitialMinistryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InitialMinistryTableViewCell.swift
+//  CruApp
+//
+//  Created by Mariel Sanchez on 1/20/16.
+//  Copyright © 2016 iCrew. All rights reserved.
+//
+
+import UIKit
+
+class InitialMinistryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

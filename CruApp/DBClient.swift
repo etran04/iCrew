4680e799db4aa8d4b60e532bae875894e59c8f10
takong15 <takong15@gmail.com>
2016-03-09 20:41:33 -0800

    database running

--- CruApp/DBClient.swift
+++ CruApp/DBClient.swift
@@ -16,7 +16,7 @@ class DBClient {
     
     func requestData(action: String, completionHandler : (NSData?, NSURLResponse?, NSError?) -> Void) {
         //let url = "http://localhost:3001/api/" + action + "/list"
-        let url = "http://pcp070608pcs.wireless.calpoly.edu:3001/api/" + action + "/list"
+        let url = "http://pcp070211pcs.wireless.calpoly.edu:3001/api/" + action + "/list"
         
         //for sorting
         //let url = http://localhost:3000/api/minstry/find?order={name: 1}
@@ -56,7 +56,7 @@ class DBClient {
     //func addData(action: String, direction : String, seats : Int, driverNumber : Int, event : String, driverName : String) {
     func addData(action : String, body: NSData) {
 
-        let url = "http://pcp070608pcs.wireless.calpoly.edu:3001/api/" + action + "/create"
+        let url = "http://pcp070211pcs.wireless.calpoly.edu:3001/api/" + action + "/create"
         //let url = "http://localhost:3001/api/" + action + "/create"
 
         sendPostRequest(url, body: body, completionHandler: emptyHandler)
@@ -67,7 +67,7 @@ class DBClient {
     }
     
     func postData(action: String, body: NSData) {
-        let url = "http://pcp070608pcs.wireless.calpoly.edu:3001/api/" + action
+        let url = "http://pcp070211pcs.wireless.calpoly.edu:3001/api/" + action
         //let url = "http://localhost:3001/api/" + action
     
         sendPostRequest(url, body: body, completionHandler: emptyHandler)
@@ -115,7 +115,7 @@ class DBClient {
     
     func addPassenger(rideId: String, action: String, body: NSData) {
         //let url = "http://localhost:3001/api/" + action + "/create"
-        let url = "http://pcp070608pcs.wireless.calpoly.edu:3001/" + action + "/create"
+        let url = "http://pcp070211pcs.wireless.calpoly.edu:3001/passenger/create"
         sendPostRequest(url, body: body, completionHandler: {(data : NSData?, response : NSURLResponse?, error : NSError?) in
             do {
                 if (data != nil) {
@@ -123,9 +123,8 @@ class DBClient {
                     let JSONData = JSONResponse as! NSDictionary
                     let post = JSONData["post"] as! NSDictionary
                     let passengerId = post["_id"] as! String
-                    print("passengerId: " + passengerId)
                     //let url = "http://localhost:3001/" + "api/ride/addPassenger"
-                    let url = "http://pcp070608pcs.wireless.calpoly.edu:3001/" + "api/ride/addPassenger"
+                    let url = "http://pcp070211pcs.wireless.calpoly.edu:3001/" + "api/ride/addPassenger"
 
                     let params = ["ride_id": rideId,"passenger_id": passengerId]
                     
@@ -143,7 +142,8 @@ class DBClient {
                 }
             }
             catch {
-                print("ERROR: Cannot connect to database")
+                print("ERROR: Cannot connect to database!!")
+                print(error)
             }
         })
     }

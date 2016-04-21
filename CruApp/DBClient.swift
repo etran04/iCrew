785c1bfc5fcf73f86ee987b3c54478c86c1edb74
takong15 <takong15@gmail.com>
2016-04-21 12:23:04 -0700

    database running on EC2

--- CruApp/DBClient.swift
+++ CruApp/DBClient.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-let databaseURL = "http://pcp070211pcs.wireless.calpoly.edu:3001/api/"
+let databaseURL = "http://ec2-52-32-197-212.us-west-2.compute.amazonaws.com:3001/api/"
+//let databaseURL = "http://pcp070211pcs.wireless.calpoly.edu:3001/api/"
 
 //let databaseURL = "http://localhost:3001/api/"
 

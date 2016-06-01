8b71b446349308e73efee051dc21407a02d46ab9
Eric Tran <erictritran@yahoo.com>
2016-06-01 16:41:18 -0700

    Offered ride cancellation added into new Rideshare UI. Click on a status and choose to cancel if wished

--- CruApp/OfferedRidesVC.swift
+++ CruApp/OfferedRidesVC.swift
@@ -190,7 +190,7 @@ class OfferedRidesVC: UIViewController, UITableViewDelegate, UITableViewDataSour
     /* Dynamically size the number of rows to match the number of statuses we have */
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return driverCollection.count
-//        return 3
+//        return 1
     }
     
     /* Loads each individual cell in the table with a offered status */
@@ -203,32 +203,65 @@ class OfferedRidesVC: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        let alert = UIAlertView()
-//        alert.delegate = self
-//        alert.title = "Passengers"
-//        let driver = driverCollection[indexPath.row] as Driver
-//            
-//        if (driver.passengers.count != 0) {
-//            var msg = ""
-//            
-//            for index in 0...(driver.passengers.count - 1) {
-//                    
-//                var number = String(driver.passengers[index].phoneNumber)
-//                number = number.insert("(", ind: 0)
-//                number = number.insert(") ", ind: 4)
-//                number = number.insert(" - ", ind: 9)
-//                
-//                msg += driver.passengers[index].name + " " + number + "\n"
-//            }
-//            alert.message = msg
-//        } else {
-//            alert.message = "No passengers at this time."
-//        }
-//        alert.addButtonWithTitle("OK")
-//        alert.show()
-        
+        displayDetailsOfferedRide(indexPath)
         tableview.deselectRowAtIndexPath(indexPath, animated: true)
 
+    }
+
+    // Helper function for viewing more information about an offered ride 
+    func displayDetailsOfferedRide(indexPath: NSIndexPath) {
+        var message: String!
+        let driver = driverCollection[indexPath.row]
+        if (driver.passengers.count != 0) {
+            var msg = ""
+    
+            for index in 0...(driver.passengers.count - 1) {
+                var number = String(driver.passengers[index].phoneNumber)
+                number = number.insert("(", ind: 0)
+                number = number.insert(") ", ind: 4)
+                number = number.insert(" - ", ind: 9)
+        
+                msg += driver.passengers[index].name + " " + number + "\n"
+            }
+            message = msg
+        } else {
+            message = "No passengers at this time."
+        }
+    
+        let alert = UIAlertController(title: "Current ride!", message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .Default) { _ in })
+        alert.addAction(UIAlertAction(title: "Cancel Ride", style: .Default) { _ in self.cancelDriver(indexPath.row)})
+        self.presentViewController(alert, animated: true){}
+    }
+    
+    /* Callback for when a cancel button is pressed in a driver cell. Input is the row of the cell at which it's pressed */
+    func cancelDriver(row: Int) {
+        let titleMsg = "Are you sure?"
+        let msg = "Are you sure you want to cancel your ride offering?"
+        
+        let confirmDialog = UIAlertController(title: titleMsg, message: msg, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "Yes", style: .Default) { (UIAlertAction) -> Void in
+            
+            let rideId = self.driverCollection[row].rideId;
+            DBClient.deleteData("rides/" + rideId)
+            
+            for pssngr in self.passengerCollection {
+                if (pssngr.rideId == self.driverCollection[row].rideId) {
+                    self.passengerCollection.removeAtIndex(row)
+                }
+            }
+            
+            self.driverCollection.removeAtIndex(row)
+            self.tableview.reloadData()
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "No", style: .Default, handler: nil)
+        
+        confirmDialog.addAction(okAction)
+        confirmDialog.addAction(cancelAction)
+        
+        self.presentViewController(confirmDialog, animated: true, completion: nil)
     }
     
     // MARK: - DZN Empty data set methods 
